1797703865b1e38ab9cb25be50959382a3bfc36e
Tom Kidd <tomjkidd@gmail.com>
2016-06-13 22:58:29 -0400

    Update lein exec template to allow dynamic m2 loading
    I encountered a blog post that gave some good pointers for
    using lein exec
    
    http://charsequence.blogspot.in/2012/04/scripting-clojure-with-leiningen-2.html
    
    The first piece of advice was to use #! /bin/sh lein exec so that
    with chmod a+x, you can run the script directly with ./clj-template.clj.
    
    Next, I worked on a Wikipedia web scraper, and want to use enlive in my
    script. In order to do that, I had to use the leiningen.exec/deps call, which
    is also added in this commit.
    
    With these changes, it should be really easy to create clojure scripts to
    use for any problems I encounter in the wild.

--- tools/clojure/cli-template.clj
+++ tools/clojure/cli-template.clj
@@ -1,6 +1,24 @@
-(require '[clojure.tools.cli :refer [parse-opts]])
+#! /bin/sh lein exec
+
+(require 'leiningen.exec)
+
+;; Add dependencies to the classpath on the fly
+;; NOTE: This requires that they are in your m2 repository already 
+(leiningen.exec/deps '[[enlive "1.1.6"]
+                       [clj-time "0.12.0"]])
+
+;; Now dependencies are available like in a regular lein project
+(require '[clojure.string :as str]
+         '[clojure.tools.cli :refer [parse-opts]]
+         '[net.cgrand.enlive-html :refer [html-resource select text first-child]]
+         '[clj-time.format :refer [formatter parse]])
+
+;;;;;;;;;;;;;;;;;;; Add your script fns here  ;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;; command line interface fns  ;;;;;;;;;;;;;;;;;;;;;;;
 
 (def cli-opts
+  "The command line options that are available for the script"
   [["-s" "--string STRING" "String Argument"
     :default "this is a default string"]
    ["-i" "--int I" "Int Argument"
@@ -8,11 +26,14 @@
     :parse-fn #(Integer/parseInt %)]])
 
 (defn -main [& args]
+  "The main function that is called with run with lein exec"
   (let [{:keys [options arguments errors summary]} (parse-opts args cli-opts)]
     (println (str "options = " options))))
 
 (try
-  (require 'leiningen.exec)
+  ; If you don't require any m2 libraries, remove the earlier calls that
+  ; dynamically load them and uncomment the following line
+  ;;(require 'leiningen.exec)
   (when @(ns-resolve 'leiningen.exec '*running?*)
     (apply -main (rest *command-line-args*)))
   (catch java.io.FileNotFoundException e))
