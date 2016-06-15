c44fad9e12eb0c8e1471dc5f30ff875da5ad65c8
Tom Kidd <tomjkidd@gmail.com>
2016-06-15 08:30:44 -0400

    Update clj-template to use clojure.tools.cli sample code
    [tools.cli](https://github.com/clojure/tools.cli) provides some good defaults
    for cli-options, error-msg, exit, and -main function definitions that make
    the script have argument and help handling.
    
    This commit adds those functions to my template.

--- tools/clojure/cli-template.clj
+++ tools/clojure/cli-template.clj
@@ -14,22 +14,84 @@
          '[net.cgrand.enlive-html :refer [html-resource select text first-child]]
          '[clj-time.format :refer [formatter parse]])
 
+(import (java.net InetAddress))
+
 ;;;;;;;;;;;;;;;;;;; Add your script fns here  ;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;; command line interface fns  ;;;;;;;;;;;;;;;;;;;;;;;
 
-(def cli-opts
+(def cli-options
   "The command line options that are available for the script"
-  [["-s" "--string STRING" "String Argument"
-    :default "this is a default string"]
-   ["-i" "--int I" "Int Argument"
-    :default 12
-    :parse-fn #(Integer/parseInt %)]])
+  [;; First three strings describe a short-option, long-option with optional
+   ;; example argument description, and a description. All three are optional
+   ;; and positional.
+   ["-p" "--port PORT" "Port number"
+    :default 80
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-H" "--hostname HOST" "Remote host"
+    :default (InetAddress/getByName "localhost")
+    ;; Specify a string to output in the default column in the options summary
+    ;; if the default value's string representation is very ugly
+    :default-desc "localhost"
+    :parse-fn #(InetAddress/getByName %)]
+   ;; If no required argument description is given, the option is assumed to
+   ;; be a boolean option defaulting to nil
+   [nil "--detach" "Detach from controlling process"]
+   ["-v" nil "Verbosity level; may be specified multiple times to increase value"
+    ;; If no long-option is specified, an option :id must be given
+    :id :verbosity
+    :default 0
+    ;; Use assoc-fn to create non-idempotent options
+    :assoc-fn (fn [m k _] (update-in m [k] inc))]
+   ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["This is my program. There are many like it, but this one is mine."
+        ""
+        "Usage: program-name [options] action"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Actions:"
+        "  start    Start a new server"
+        "  stop     Stop an existing server"
+        "  status   Print a server's status"
+        ""
+        "Please refer to the manual page for more information."]
+       (str/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (str/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
 
 (defn -main [& args]
-  "The main function that is called with run with lein exec"
-  (let [{:keys [options arguments errors summary]} (parse-opts args cli-opts)]
-    (println (str "options = " options))))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    ;; Handle help and error conditions
+    (cond
+      (:help options) (exit 0 (usage summary))
+      ; Uncomment if you need to process arguments
+      ;(not= (count arguments) 1) (exit 1 (usage summary))
+      errors (exit 1 (error-msg errors)))
+
+    (println (str "options = " options))
+    
+    ;;;;;;;;;;;;;;;;;;; Hook into your script fns here  ;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;; Execute program with options
+    ; Uncomment if you need to handle arguments
+    ;; (case (first arguments)
+    ;;   "start" (println "Note implemented. TODO: make call to start function.")
+    ;;   "stop" (println "Not implemented. TODO: make call to stop function.")
+    ;;   "status" (println "Not implemented. TODO: make call to status function.")
+    ;;   (exit 1 (usage summary)))
+
+    ))
 
 (try
   ; If you don't require any m2 libraries, remove the earlier calls that
